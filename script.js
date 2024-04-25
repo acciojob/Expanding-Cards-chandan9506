02f96e2b80fc63b9e599867e22cdc197eba048ba
chandan9506 <152258839+chandan9506@users.noreply.github.com>
2024-04-25 08:24:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+    const panels = document.querySelectorAll(".panel");
+  
+    panels.forEach(function(panel, index) {
+      panel.addEventListener("click", function() {
+        // Remove active class from all panels
+        panels.forEach(function(panel) {
+          panel.classList.remove("active");
+        });
+        // Add active class to clicked panel
+        this.classList.add("active");
+      });
+    });
+  });
